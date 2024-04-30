b0f7e1ec82fd1e949b64a4532a1e3e778148c877
Paarth Kukreja <paarthkukreja@outlook.com>
2024-04-30 18:20:41 +0400

    image change

--- src/sections/AboutSection/AboutSection.jsx
+++ src/sections/AboutSection/AboutSection.jsx
@@ -41,7 +41,7 @@ export default function AboutSection() {
         <div className={styles.aboutImageCont}>
           <img
             src={
-              "https://res.cloudinary.com/dce66e96q/image/upload/f_auto,q_auto,w_1000/v1/about-section/tof6"
+              "https://res.cloudinary.com/dce66e96q/image/upload/f_auto:image,q_auto,w_1000/v1/about-section/tof6"
             }
             className={styles.aboutImage}
             alt={"Tachyons Finals Car"}
--- src/sections/Sponsors-Partners/Sponsors-Partners.jsx
+++ src/sections/Sponsors-Partners/Sponsors-Partners.jsx
@@ -3,34 +3,34 @@ import styles from "./Sponsors-Partners.module.scss";
 export default function SponsorsPartners() {
   const sponsors = [
     {
-      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto,q_auto,w_600/v1/sponsors%2Bpartners/expert-ai",
+      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto:image,q_auto,w_600/v1/sponsors%2Bpartners/expert-ai",
       alt: "Expert AI",
       link: "https://expertai-lux.com/",
     },
     {
-      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto,q_auto,w_600/v1/sponsors%2Bpartners/dubby",
+      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto:image,q_auto,w_600/v1/sponsors%2Bpartners/dubby",
       alt: "Dubby",
       link: "https://www.dubby.gg/",
     },
     {
-      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto,q_auto,w_600/v1/sponsors%2Bpartners/quoffee",
+      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto:image,q_auto,w_600/v1/sponsors%2Bpartners/quoffee",
       alt: "Quoffee",
       link: "",
     },
     {
-      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto,q_auto,w_600/v1/sponsors%2Bpartners/historic-future",
+      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto:image,q_auto,w_600/v1/sponsors%2Bpartners/historic-future",
       alt: "Historic Future",
       link: "",
     },
     {
-      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto,q_auto,w_600/v1/sponsors%2Bpartners/trattoria-rosmarino",
+      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto:image,q_auto,w_600/v1/sponsors%2Bpartners/trattoria-rosmarino",
       alt: "Trattoria Rosmarino",
       link: "https://www.trattoriarosmarino.it/",
     },
   ];
   const partners = [
     {
-      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto,q_auto,w_600/v1/sponsors%2Bpartners/uniofg",
+      src: "https://res.cloudinary.com/dce66e96q/image/upload/f_auto:image,q_auto,w_600/v1/sponsors%2Bpartners/uniofg",
       alt: "University of Genoa",
       link: "https://unige.it/en",
     },
