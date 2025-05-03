efbcae8dacbc5155c4c44b3c588308e05a6fd0e2
Paarth Kukreja <paarthkukreja@outlook.com>
2025-05-03 16:30:32 +0400

    tachyons.eu is gone :( its now tachyons.paarthkureja.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tachyons.eu",
+  site: "https://tachyons.paarthkureja.com",
   integrations: [preact(), sitemap(), vue()],
   adapter: cloudflare(),
 });
